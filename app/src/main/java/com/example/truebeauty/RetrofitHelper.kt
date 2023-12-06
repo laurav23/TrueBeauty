25e636afa769c46eb449d201607322e538b93ec5
Laura Vidal <lauracatalina019@gmail.com>
2023-12-05 22:45:28 -0500

    Ultima actualización

--- app/src/main/java/com/example/truebeauty/RetrofitHelper.kt
+++ app/src/main/java/com/example/truebeauty/RetrofitHelper.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
-    private const val BASE_URL = "http://192.168.0.6:8000"
+    private const val BASE_URL = "http://10.185.208.153:8000"
 
     fun getInstance(): Retrofit {
         return Retrofit.Builder()
